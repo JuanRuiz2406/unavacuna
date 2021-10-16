6fa3f43149c4f2014cb786a95c1849a625ceebfc
Elvin González <60045534+elvinlab@users.noreply.github.com>
2021-10-15 21:24:26 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 require("dotenv").config();
 
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   webpack: (config) => {
     const env = Object.keys(process.env).reduce((acc, curr) => {
       acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
